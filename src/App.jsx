f7ea488a85f0813fd76fb7343b7e366636e95e80
MBMAZAM <maza_1506@hotmail.com>
2023-12-13 02:14:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+
+      <ListadoPacientes/>
+
+      <Formulario/>
+      <h1>Hola Mundo</h1>
+      
+    </>
+  )
+}
+
+export default App
